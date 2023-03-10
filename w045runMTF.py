a51a702f820c343d36e87bcc8b72fcf7b3e481cf
tradyai <tobby2002@gmail.com>
2023-03-10 23:34:44 +0900

    Update w045runMTF.py

--- w045runMTF.py
+++ w045runMTF.py
@@ -1457,8 +1457,8 @@ if __name__ == '__main__':
     logger.info('PID:' + str(os.getpid()))
     logger.info('seq:' + seq)
     while True:
-        # if i % 10 == 1:
-        logger.info(f'{i} start: {time.strftime("%H:%M:%S")}')
+        if i % 10 == 1:
+            logger.info(f'{i} start: {time.strftime("%H:%M:%S")}')
         single(symbols, i)
         i += 1
     print(f'Finished in {round(time.perf_counter() - start, 2)} second(s)')
